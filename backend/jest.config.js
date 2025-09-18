7ae046f38047f59c00bd953c08c0ea6eb91b9e2e
Thepushpendra68 <pushpendrachl@gmail.com>
2025-09-18 11:38:12 +0530

    Fix: Add Jest configuration file to properly handle test discovery

---
+++ backend/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: [
+    '**/tests/**/*.test.js',
+    '**/tests/**/*.spec.js',
+    '**/__tests__/**/*.test.js',
+    '**/__tests__/**/*.spec.js'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js',
+    '!src/**/*.spec.js'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  passWithNoTests: true
+};
